d2b74b79da5fdf2304a2e10e3f1a5cb975f252e4
Victor-Luizz <victorrluizz2004@gmail.com>
2026-05-26 17:41:12 -0300

    Delete vitinho.py

--- vitinho.py
+++
@@ -1,6 +0,0 @@
-usuario = str(input("Informe o seu nome : "))
-senha = int(input("Informe sua senha : "))
-if usuario == "victor" and senha == 1234:
-    print("Acesso liberado")
-else:
-    print("Erro ao logar ! ")    
